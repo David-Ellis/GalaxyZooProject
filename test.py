4e671d97131a2a1349143a08f33d613f9d169646
c.luehrmann <c.luehrmann@stud.uni-goettingen.de>
2020-04-28 17:05:13 +0200

    new changes

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
 print('This is a test')
-print('Johanna is making changes to this file.')
+print('Johanna is cool and making changes to this file.')
 print('Machine learning is awesome!')
+
+def testfunction(x):
+	return x**x
+print(testfunction(102))
+
