e49d9d19aaa94e01f2dd870740e2701f6efadb8d
David-Ellis <david.ellis94@gmail.com>
2020-05-07 16:27:18 +0200

    Added to test.py

--- test.py
+++ test.py
@@ -5,8 +5,12 @@ print('Machine learning is awesome!')
 
 def testfunction(x):
 	return x**x
+	
 print(testfunction(102))
-
 
 print('Test: What happens if we make changes simultaneously?')
 
+print("\n It should try to merge the changes together automatically.")
+
+while True:
+	print("'WhatsApp is lame, cool kids use GitHub.' - Dave 2020")
