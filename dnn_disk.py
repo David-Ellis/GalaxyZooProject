c975a46a3d5074ba9173f553a7d3e8a7c7ab3a7b
David-Ellis <david.ellis94@gmail.com>
2020-06-20 19:37:19 +0200

    Added script to help determine best number of neurons in first and second layer

--- dnn_disk.py
+++ dnn_disk.py
@@ -12,6 +12,12 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+import matplotlib
+
+matplotlib.rcParams['mathtext.fontset'] = 'stix'
+matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.rc('font', size=18)
+
 plt.close()
 
 seed=0
@@ -101,13 +107,14 @@ from keras.layers import Dense, Dropout#, Flatten
 #from keras.layers import Conv2D, MaxPooling2D
 
 
-def create_DNN(neurons1):
+def create_DNN(neurons1, neurons2):
     # instantiate model
     model = Sequential()
     # add a dense all-to-all relu layer
     model.add(Dense(neurons1,input_shape=(img_rows*img_cols,), activation='relu'))
     # add a dense all-to-all relu layer
-    model.add(Dense(100, activation='relu'))
+    if neurons2 > 0:
+      model.add(Dense(100, activation='relu'))
     # apply dropout with rate 0.5
     model.add(Dropout(0.5))
     # soft-max layer
@@ -118,9 +125,10 @@ print('Model architecture created successfully!')
 
 # %% Choose the Optimizer and the Cost Function
 
-def compile_model(optimizer=keras.optimizers.Adadelta(), neurons1 = 400):
+def compile_model(optimizer=keras.optimizers.Adadelta(), neurons1 = 600,
+                  neurons2 = 100):
     # create the mode
-    model=create_DNN(neurons1)
+    model=create_DNN(neurons1, neurons2)
     # compile the model
     model.compile(loss=keras.losses.categorical_crossentropy,
                   optimizer=optimizer,
@@ -180,7 +188,7 @@ plt.show()
 # Seems to be different every time
 
 tf.random.set_seed(seed)
-epochs = 5
+epochs = 6
 
 optimizers = [keras.optimizers.SGD(),
               keras.optimizers.RMSprop(), keras.optimizers.Adagrad(), 
@@ -190,7 +198,7 @@ optimizers = [keras.optimizers.SGD(),
 test_accuracy = np.zeros(len(optimizers))
 for i, optimizer in enumerate(optimizers):
 
-  model_DNN=compile_model(optimizer = optimizer)
+  model_DNN=compile_model(optimizer = optimizer, neurons1 = 600)
   
   # train DNN and store training info in history
   history=model_DNN.fit(X_train, Y_train,
@@ -203,8 +211,9 @@ for i, optimizer in enumerate(optimizers):
   test_accuracy[i] = score[1]
   
 plt.figure()  
-plt.bar(range(len(optimizers)),test_accuracy, 
-        label = ["SGD", "RMSprop", "Adagrad", "Adadelta", "Adamax", "Adam", "Nadam"])
+plt.bar(range(len(optimizers)),test_accuracy)
+labels = ["SGD", "RMSprop", "Adagrad", "Adadelta", "Adamax", "Adam", "Nadam"]
+plt.xticks(range(len(optimizers)), labels)
 plt.ylabel("Accuracy on Test Data")
 plt.ylim(0.9, 1)
 plt.xlabel("Optimiser")
@@ -217,31 +226,74 @@ import time
 # training parameters
 batch_size = 64
 epochs = 5
+retest = 10
 
-neurons_list = np.arange(50, 1000, 100)
-test_accuracy = np.zeros(len(neurons_list))
+neurons_list1 = np.linspace(50, 1000, 10)
+neurons_list2 = np.array([0, 200, 400])
 
-for i, neurons in enumerate(neurons_list):
-  tic = time.process_time()
-  # create the deep neural net
-  model_DNN=compile_model(optimizer = keras.optimizers.Nadam(), neurons1 = neurons)
+for j, neurons2 in enumerate(neurons_list2):
+  test_accuracy_mean = np.zeros((len(neurons_list1)))
+  test_accuracy_upper = np.zeros((len(neurons_list1)))
+  test_accuracy_lower = np.zeros((len(neurons_list1)))
   
-  # train DNN and store training info in history
-  history=model_DNN.fit(X_train, Y_train,
-            batch_size=batch_size,
-            epochs=epochs,
-            verbose=0,
-            validation_data=(X_test, Y_test))
+  for i, neurons1 in enumerate(neurons_list1):
+    tic = time.perf_counter()
+    # create the deep neural net
+    model_DNN=compile_model(optimizer = keras.optimizers.Nadam(), 
+                            neurons1 = int(neurons1), neurons2 = neurons2)
+    
+    # train DNN and store training info in history
+    accuracies = np.zeros(retest)
+    
+    for j in range(retest):
+      history=model_DNN.fit(X_train, Y_train,
+                batch_size=batch_size,
+                epochs=epochs,
+                verbose=0,
+                validation_data=(X_test, Y_test))
+      
+      score = model_DNN.evaluate(X_test, Y_test, verbose=0)
+      accuracies[j] = score[1]
+      
+    test_accuracy_mean[i] = np.median(accuracies)
+    test_accuracy_upper[i] = np.percentile(accuracies, 84.1)
+    test_accuracy_lower[i] = np.percentile(accuracies, 25.9)
+    toc = time.perf_counter()
+    print("finished {} in {} seconds".format(i, toc-tic))
+  np.save("data/num_neurons_test_{}n2".format(neurons2), 
+          [neurons_list1, test_accuracy_mean, test_accuracy_upper, 
+           test_accuracy_lower])
+    
   
-  score = model_DNN.evaluate(X_test, Y_test, verbose=0)
-  test_accuracy[i] = score[1]
-  toc = time.process_time()
-  print("finished {} in {} seconds".format(i, toc-tic))
-  
+#%% Plot
+
+line_colors = ["b", "#ffcc00", "g"]
+error_colors = ["#99bbff", "#ffe066", "#80ffaa"]
+
 plt.figure()
-plt.plot(neurons_list,test_accuracy, )
+for j, neurons2 in enumerate(neurons_list2):
+  neurons_list1, test_accuracy_mean, test_accuracy_upper, test_accuracy_lower = np.load("data/num_neurons_test_{}n2.npy".format(neurons2))
+  plt.fill_between(neurons_list1, test_accuracy_lower, test_accuracy_upper, 
+                 color = error_colors[j], alpha = 0.5)
+  plt.plot(neurons_list1, test_accuracy_mean, "-o", lw = 2, 
+           color = line_colors[j], label = "$n_2 = {}$".format(neurons2))
+  plt.plot(neurons_list1, test_accuracy_lower, "v", lw = 2, 
+           color = line_colors[j])
+  plt.plot(neurons_list1, test_accuracy_upper, "^", lw = 2, 
+           color = line_colors[j])
+
+plt.legend()
 plt.ylabel("Accuracy on Test Data")
 plt.ylim(0.9, 1)
-plt.xlabel("# Neutrons in first Dense Layer")
-
+plt.xlabel("# Neutrons in first Dense Layer, $n_1$")
+plt.tight_layout()
+plt.savefig("figures/disk_num_neurons_noSecondLayer.pdf")
 #print("Optimiser {} performs the best".format(np.arange(len(optimizers))[test_accuracy == max(test_accuracy)]))
+
+
+#%% time test
+
+tic = time.perf_counter()
+time.sleep(3)
+toc = time.perf_counter()
+print(toc-tic)
