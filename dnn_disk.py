f5e0c3b9ae7ded24df86d47e08406f892087e130
David-Ellis <david.ellis94@gmail.com>
2020-06-21 16:54:48 +0200

    Added section to help determine best number of dense layers

--- dnn_disk.py
+++ dnn_disk.py
@@ -117,8 +117,7 @@ def create_DNN(neurons1, neurons2):
       model.add(Dense(neurons1,input_shape=(img_rows*img_cols,), activation='relu'))
     # add a dense all-to-all relu layer
     if neurons2 > 0:
-      model.add(Dense(100, activation='relu'))
-      
+      model.add(Dense(neurons2, activation='relu'))
     # apply dropout with rate 0.5
     model.add(Dropout(0.5))
     # soft-max layer
@@ -284,17 +283,113 @@ for j, neurons2 in enumerate(neurons_list2):
                  color = error_colors[j], alpha = 0.5)
   plt.plot(neurons_list1, test_accuracy_mean, "-o", lw = 2, 
            color = line_colors[j], label = "$n_2 = {}$".format(neurons2))
-  plt.plot(neurons_list1, test_accuracy_lower, "v", lw = 2, 
-           color = line_colors[j])
-  plt.plot(neurons_list1, test_accuracy_upper, "^", lw = 2, 
-           color = line_colors[j])
+  plt.plot(neurons_list1, test_accuracy_lower, ":", lw = 2, 
+           color = error_colors[j])
+  plt.plot(neurons_list1, test_accuracy_upper, ":", lw = 2, 
+           color = error_colors[j])
 
 plt.legend()
 plt.ylabel("Accuracy on Test Data")
 plt.ylim(0.9, 1)
-plt.xlabel("# Neutrons in first Dense Layer, $n_1$")
+plt.xlabel("# Neurons in first Dense Layer, $n_1$")
 plt.tight_layout()
-plt.savefig("figures/disk_num_neurons_noSecondLayer.pdf")
-#print("Optimiser {} performs the best".format(np.arange(len(optimizers))[test_accuracy == max(test_accuracy)]))
+plt.savefig("figures/disk_num_neurons.pdf")
 
 
+#%% Investigate impact of having more layers
+
+def create_DNN2(layers, neurons):
+    # instantiate model
+    model = Sequential()
+    # add a dense all-to-all relu layer
+    if layers > 0:
+      model.add(Dense(neurons,input_shape=(img_rows*img_cols,), activation='relu'))
+      # layers-1 more dense all-to-all relu layer
+      for i in range(layers-1):
+        model.add(Dense(neurons, activation='relu'))
+      
+    # apply dropout with rate 0.5
+    model.add(Dropout(0.5))
+    # soft-max layer
+    model.add(Dense(num_classes, activation='softmax'))
+    return model
+
+print('Model architecture created successfully!')
+
+# Choose the Optimizer and the Cost Function
+def compile_model2(optimizer=keras.optimizers.Adadelta(), layers = 2,
+                  neurons = 200):
+    # create the mode
+    model=create_DNN2(layers, neurons)
+    # compile the model
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=optimizer,
+                  metrics=['accuracy'])
+    return model
+  
+#%%
+# training parameters
+batch_size = 64
+epochs = 5
+retest = 10
+
+layers_list = np.array([1,2,3,4,5,6,7])
+
+test_accuracy_mean = np.zeros((len(layers_list)))
+test_accuracy_upper = np.zeros((len(layers_list)))
+test_accuracy_lower = np.zeros((len(layers_list)))
+
+for i, layers in enumerate(layers_list): 
+  tic = time.perf_counter()
+  # create the deep neural net
+  model_DNN=compile_model2(optimizer = keras.optimizers.Nadam(), 
+                           layers = layers, neurons = 200)
+  
+  # train DNN and store training info in history
+  accuracies = np.zeros(retest)
+  for j in range(retest):
+    # Get new data split
+    X_train, Y_train, X_test, Y_test = splitData(X, Y, trainingPercentage)
+    
+    history=model_DNN.fit(X_train, Y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=0,
+              validation_data=(X_test, Y_test))
+    score = model_DNN.evaluate(X_test, Y_test, verbose=0)
+    accuracies[j] = score[1]
+  
+  #print(i, accuracies, np.median(accuracies))   
+  test_accuracy_mean[i] = np.median(accuracies)
+  test_accuracy_upper[i] = np.percentile(accuracies, 84.1)
+  test_accuracy_lower[i] = np.percentile(accuracies, 25.9)
+  toc = time.perf_counter()
+  print("finished {} in {:.3} seconds".format(i, toc-tic))
+  
+np.save("data/disk_num_layers_test", 
+          [layers_list, test_accuracy_mean, test_accuracy_upper, 
+           test_accuracy_lower])
+
+#%% Plot
+
+line_colors = ["b", "#ffcc00", "g"]
+error_colors = ["#99bbff", "#ffe066", "#80ffaa"]
+
+plt.figure()
+
+layers_list, test_accuracy_mean, test_accuracy_upper, test_accuracy_lower \
+  = np.load("data/num_layers_test.npy")
+plt.fill_between(layers_list, test_accuracy_lower, test_accuracy_upper, 
+                 color = error_colors[0], alpha = 0.5)
+plt.plot(layers_list, test_accuracy_mean, "-o", lw = 2, 
+           color = line_colors[0])
+plt.plot(layers_list, test_accuracy_lower, ":", lw = 2, 
+           color = error_colors[0])
+plt.plot(layers_list, test_accuracy_upper, ":", lw = 2, 
+           color = error_colors[0])
+
+plt.ylabel("Accuracy on Test Data")
+plt.ylim(0.95, 1)
+plt.xlabel("# Layers")
+plt.tight_layout()
+plt.savefig("figures/disk_num_layers.pdf")
